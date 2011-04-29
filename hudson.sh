536931e2eb3618efc324e54b3e496f97effebe8c
Puneet Goyal <pungoyal@gmail.com>
2011-04-29 02:35:13 -0700

    Edited hudson.sh via GitHub

--- hudson.sh
+++ hudson.sh
@@ -1,6 +1,6 @@
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 echo "deb http://pkg.jenkins-ci.org/debian binary/" > jenkins.list
-echo "deb http://archive.canonical.com/ubuntu maverick partner" > jdk.list
+echo "deb http://archive.canonical.com/ubuntu natty partner" > jdk.list
 sudo mv *.list /etc/apt/sources.list.d/
 
 sudo aptitude update
