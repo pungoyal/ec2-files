fecaf77606d81d0cb3e8883cd2dac6893e68d5e8
Puneet Goyal <pungoyal@gmail.com>
2011-04-29 12:26:07 +0530

    hudson

---
+++ hudson.sh
@@ -0,0 +1,8 @@
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+echo "deb http://pkg.jenkins-ci.org/debian binary/" > jenkins.list
+sudo mv jenkins.list /etc/apt/sources.list.d/
+
+sudo aptitude update
+sudo aptitude -y safe-upgrade
+sudo aptitude install -y jenkins apache2
+
